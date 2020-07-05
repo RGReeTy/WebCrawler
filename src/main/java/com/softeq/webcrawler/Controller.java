e3b5f722b1a9fdd9fbf47f87b828791fd150962f
RGReeTy <be.a.reety@gmail.com>
2020-07-05 20:41:31 +0300

    add some code (still working)

--- src/main/java/com/softeq/webcrawler/Controller.java
+++ src/main/java/com/softeq/webcrawler/Controller.java
@@ -5,9 +5,7 @@ import com.softeq.webcrawler.util.SearchForMatches;
 import com.softeq.webcrawler.util.SearchForMatchesRegexImpl;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 import static com.softeq.webcrawler.util.JsoupParser.parseHTMLBody;
 
@@ -15,18 +13,26 @@ public class Controller {
 
     public void startWebScrapping(ConfigParam configParam) {
 
-        List<String> inputWords = new ArrayList<>(Arrays.asList(configParam.getWordsToFind().split(",")));
         SearchForMatches searchForMatches = new SearchForMatchesRegexImpl();
 
+        List<String> inputWords = new ArrayList<>(Arrays.asList(configParam.getWordsToFind().split(",")));
+        //TODO concurrent queue
+        Deque<String> reference = new ArrayDeque<String>();
+        reference.addFirst(configParam.getUrl());
+
+
         try {
-            String bodyOfHtml = parseHTMLBody(configParam.getUrl());
-            StringBuilder lineOfCounters = new StringBuilder(configParam.getUrl());
+            while (!reference.isEmpty()) {
+                String bodyOfHtml = parseHTMLBody(reference.getFirst());
+                StringBuilder lineOfCounters = new StringBuilder(configParam.getUrl());
 
-            for (String word : inputWords) {
-                lineOfCounters.append(" ").append(searchForMatches.countMatchesForText(word, bodyOfHtml));
-
+                for (String word : inputWords) {
+                    lineOfCounters.append(" ").append(searchForMatches.countMatchesForText(word, bodyOfHtml));
+                }
+                System.out.println(lineOfCounters);
+                reference.removeFirst();
             }
-            System.out.println(lineOfCounters);
+
 
         } catch (IOException e) {
             //TODO add logger
--- src/main/java/com/softeq/webcrawler/util/JsoupParser.java
+++ src/main/java/com/softeq/webcrawler/util/JsoupParser.java
@@ -13,4 +13,6 @@ public class JsoupParser {
 
         return document.body().text();
     }
+
+
 }
